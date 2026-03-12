ef8517f73a5ca49f559bc9965a67974e75d680f6
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-03-12 13:17:44 -0300

    Added constants for conversion functions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 //Constants for convertion, using metric as the baseline
+const kg = 1;
+const lbs = 0.4535924;
 
-//Function for convertion
+const cm = 1;
+const inch = 2.54;
+const meter = 100;
+const yard = 91.44;
+
+const km = 1;
+const mile = 1.609344;
+
+//Function for unit convertion
+
+//Function for temperature conversion
