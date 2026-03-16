a66fe249894d360592c164a9e6d8ec8c99808ebe
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-03-16 14:54:18 -0300

    Added function to convert temperature

--- js/index.js
+++ js/index.js
@@ -13,3 +13,19 @@ const mile = 1.609344;
 //Function for unit convertion
 
 //Function for temperature conversion
+function tempConvert(input, output, value){
+    let result;
+    if(input == "c" && output == "k"){
+        result = value - 273;
+    } else if(input == "k" && output == "c"){
+        result = value + 273;
+    } else if(input == "c" && output == "f"){
+        result = (value - 32) * (1/1.8);
+    } else if(input == "f" && output == "c"){
+        result = (value*1.8) + 32;
+    } else{
+        result = null;
+    }
+
+    return result;
+}
