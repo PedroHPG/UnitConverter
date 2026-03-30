c115e0d2e0ff1810b814600285660f981060ac1d
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-03-30 14:37:01 -0300

    Adjusted functions to use auxiliary getters

--- js/index.js
+++ js/index.js
@@ -14,16 +14,45 @@ const units = {
 
 }
 
+
+//Function to get the units for conversion
+function getUnits(){
+
+}
+
+//Function to get the currently input value
+function getValue(){
+
+}
+
+//Function to swap the current units
+function swapValues(){
+
+}
+
 //Function for unit convertion
-function convert(value, unit1, unit2){
+function convert(){
+    let value = getValue();
+    let currentUnits = getUnits();
+
+    let unit1 = currentUnits[0];
+    let unit2 = currentUnits[1];
+
     let rate1 = units[unit1];
     let rate2 = units[unit2];
     return (value * rate1) / rate2;
 }
 
 //Function for temperature conversion
-function tempConvert(input, output, value){
+function tempConvert(){
     let result;
+
+    let value = getValue();
+    let currentUnits = getUnits();
+
+    let input = currentUnits[0];
+    let output = currentUnits[1];
+
     if(input == "c" && output == "k"){
         result = value - 273;
     } else if(input == "k" && output == "c"){
